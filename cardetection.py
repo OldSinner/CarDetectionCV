3c8dc967bff6f217918b39e891bae1d559552697
OldSinner <antylifepatia@gmail.com>
2024-08-11 23:22:34 +0200

    Marking area

--- cardetection.py
+++ cardetection.py
@@ -9,12 +9,26 @@ class StableCameraCarDetector:
         cap = cv2.VideoCapture(self.path)
         while True:
             ret, frame = cap.read()
-            # Processing
-            mask = self.masker.apply(frame)
+
+            # extract POI
+            roi = frame[340:650, 500:700]
+
+            # Processing Mask
+            mask = self.masker.apply(roi)
+            cts, _ = cv2.findContours(mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            
+            # ShowCont
+            for ct in cts:
+                # area 
+                area = cv2.contourArea(ct)
+                if area > 100:
+                    cv2.drawContours(roi,[ct],-1,(255,0,0),2)
 
             # Display
             cv2.imshow("Frame",frame)
-            cv2.imshow("Mask", mask)
+            # cv2.imshow("Mask", mask)
+            cv2.imshow("POI", roi)
+
 
             # Close Display
             key = cv2.waitKey(30)
